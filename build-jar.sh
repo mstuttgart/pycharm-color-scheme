13ad68c889abca5a16168a878cd543c328579855
Michell Stuttgart <michellstut@gmail.com>
2017-09-24 11:30:56 -0300

    [NEW] Add build-jar.sh file

---
+++ build-jar.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Create color folder
+COLOR_FOLDER=colors
+mkdir ${COLOR_FOLDER}
+
+# Copy the .icls files to colors folder
+cp jetbrains/*.icls ${COLOR_FOLDER}
+cp jetbrains/contrast/*.icls ${COLOR_FOLDER}
+cp jetbrains/contrast/*.icls ${COLOR_FOLDER}
+cp mythemes/*.icls ${COLOR_FOLDER}
+
+# Create an empty "IntelliJ IDEA Global Settings" file,
+# needed to be able to import the JAR using "Import Settings..."
+touch "IntelliJ IDEA Global Settings"
+jar cfM settings.jar "IntelliJ IDEA Global Settings" ${COLOR_FOLDER}
+
+# Remove temporary folders and files
+rm -rf colors
+rm "IntelliJ IDEA Global Settings"
+
